78ffc7dfe6efd484ffceb924f8f540ba782e5ff0
Martín M <skydiver@users.noreply.github.com>
2021-11-20 20:27:09 -0300

    warn on missing vercel token

--- projects.py
+++ projects.py
@@ -4,12 +4,23 @@ import sys
 import os
 import json
 
-from parser import projects
+ACCESS_TOKEN = os.getenv('ACCESS_TOKEN', None)
 
-# GET PROJECTS LIST
-SEARCH = sys.argv[1] if len(sys.argv) >= 2 else None
-projects = projects(search=SEARCH)
+if ACCESS_TOKEN is None or len(ACCESS_TOKEN) == 0:
+    data = [{
+        'title': 'Access Token not found!',
+        'subtitle': 'You need to get a Vercel API token. Please, read workflow instructions.',
+        'arg': 'github.com/skydiver/alfred-vercel',
+        'icon': {
+            'path': 'icon-warning.png'
+        }
+    }]
+else:
+    # GET PROJECTS LIST
+    from parser import projects
+    SEARCH = sys.argv[1] if len(sys.argv) >= 2 else None
+    data = projects(search=SEARCH)
 
 # RETURN ALFRED OBJECT
-data = json.dumps({ "items": projects }, indent=2)
+data = json.dumps({ "items": data }, indent=2)
 print(data)
